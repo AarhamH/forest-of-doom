5f04b9a15672bf96da157003cad784c8b287e44f
Aarham Haider <aarham.haider@gmail.com>
2022-08-18 19:32:16 -0700

    Patch 1
    -Destroyed previous Enemy AI versions and implemented final one
    -created NavMesh game object to house NavMesh features
    -created player and ground layermasks
    -downloaded and imported NavMesh component from internet

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -11,75 +10,109 @@ public class EnemyController : MonoBehaviour
 
     public LayerMask whatIsGround, whatIsPlayer;
 
+    public float health;
+
+    //Patroling
     public Vector3 walkPoint;
     bool walkPointSet;
     public float walkPointRange;
-    bool alreadyAttacked;
 
+    //Attacking
     public float timeBetweenAttacks;
+    bool alreadyAttacked;
+    public GameObject projectile;
 
-    public float sightRange,attackRange;
+    //States
+    public float sightRange, attackRange;
     public bool playerInSightRange, playerInAttackRange;
 
-
-     private void Awake() {
+    private void Awake()
+    {
+        player = GameObject.Find("Player").transform;
         agent = GetComponent<NavMeshAgent>();
-     }
-     
-     private void Update() {
-        ChasePlayer();
+    }
+
+    private void Update()
+    {
+        //Check for sight and attack range
         playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
-        playerInAttackRange = Physics.CheckSphere(transform.position, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
 
-        if(!playerInSightRange && !playerInAttackRange) Patrolling();
-        if(playerInSightRange && !playerInAttackRange) ChasePlayer();
-        if(playerInSightRange && playerInAttackRange) AttackPlayer();
+        if (!playerInSightRange && !playerInAttackRange) Patroling();
+        if (playerInSightRange && !playerInAttackRange) ChasePlayer();
+        if (playerInAttackRange && playerInSightRange) AttackPlayer();
+    }
 
-     }
+    private void Patroling()
+    {
+        if (!walkPointSet) SearchWalkPoint();
 
-     private void Patrolling(){
-        if (!walkPointSet){
-            SearchWalkPoint();
-        }
-
-        if(walkPointSet){
+        if (walkPointSet)
             agent.SetDestination(walkPoint);
-        }
 
         Vector3 distanceToWalkPoint = transform.position - walkPoint;
 
-        if(distanceToWalkPoint.magnitude < 1f){
+        //Walkpoint reached
+        if (distanceToWalkPoint.magnitude < 1f)
             walkPointSet = false;
-        }
-
-     }
-
-     private void SearchWalkPoint(){
+    }
+    private void SearchWalkPoint()
+    {
+        //Calculate random point in range
         float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
 
-        walkPoint = new Vector3(transform.position.x + randomZ, transform.position.y, transform.position.z + randomZ);
-        if(Physics.Raycast(walkPoint, - transform.up, 2f, whatIsGround)){
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
             walkPointSet = true;
-        }
-     }
+    }
 
-     private void ChasePlayer(){
+    private void ChasePlayer()
+    {
         agent.SetDestination(player.position);
-     }
+    }
 
-     private void AttackPlayer(){
+    private void AttackPlayer()
+    {
+        //Make sure enemy doesn't move
         agent.SetDestination(transform.position);
 
         transform.LookAt(player);
 
-        if(!alreadyAttacked){
-            //Attack Code Here
+        if (!alreadyAttacked)
+        {
+            ///Attack code here
+            Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
+            rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
+            rb.AddForce(transform.up * 8f, ForceMode.Impulse);
+            ///End of attack code
+
             alreadyAttacked = true;
             Invoke(nameof(ResetAttack), timeBetweenAttacks);
         }
-     }
-
-    private void ResetAttack(){
+    }
+    private void ResetAttack()
+    {
         alreadyAttacked = false;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        if (health <= 0) Invoke(nameof(DestroyEnemy), 0.5f);
+    }
+    private void DestroyEnemy()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
     }
 }
